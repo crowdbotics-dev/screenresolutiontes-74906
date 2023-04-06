15d9357d43b4aa2d790842b6153e101486751244
scartag <tag@crowdbotics.com>
2023-04-06 14:53:14 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "screenresolutiontes-74906",
-  url: "https://screenresolutiontes-74906.botics.co",
-  api: "https://screenresolutiontes-74906.botics.co/api/v1"
+  "name": "screenresolutiontes-74906",
+  "url": "https://screenresolutiontes-74906.botics.co",
+  "api": "https://screenresolutiontes-74906.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
